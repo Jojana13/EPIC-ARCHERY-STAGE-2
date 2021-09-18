38cd6ecfe19e93d4ae7c1c808b1d82fdd0a0228c
Jojana13 <86811860+Jojana13@users.noreply.github.com>
2021-09-18 11:30:36 -0400

    Add files via upload

---
+++ PlayerArrow.js
@@ -0,0 +1,41 @@
+class PlayerArrow {
+    constructor(x, y, width, height, archerAngle) {
+      var options = {
+        restitution: 0.8,
+        friction: 1.0,
+        density: 0.1,
+        isStatic: true
+      };
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      this.image = loadImage("./assets/arrow.png");
+      this.velocity = 0
+      World.add(world, this.body);
+    }
+  
+    shoot(archerAngle) {
+        archerAngle += 90;
+       
+        this.velocity = p5.Vector.fromAngle(archerAngle*(3.14/180));
+        velocity.mult(0.5);
+        Matter.Body.setVelocity(this.body, { 
+            x: velocity.x*(180/3.14),
+            y: velocity.y*(180/3.14)
+        });
+        Matter.Body.setStatic(this.body, false);
+    }
+
+   display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+
+  }
